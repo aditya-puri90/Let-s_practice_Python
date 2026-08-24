e15ad8a81cb9e1ae611c1c4a1b8b3c16c7d9e428
aditya puri <puriaditya90@gmail.com>
2026-08-24 21:01:35 +0530

    feat: Add program 21 circle area and circumference calculator

---
+++ Basic/21_circle_area_and_circumference.py
@@ -0,0 +1,12 @@
+'''Take the radius from the user.
+
+Calculate:
+
+Area = π × r²
+Circumference = 2 × π × r
+
+Use 3.14 for π.'''
+rad = int(input("Enter the radius of circle"))
+
+print(" Area :", 3.14*rad**2)
+print(" Circumferance :", 2*3.14*rad)
