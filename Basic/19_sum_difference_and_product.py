bd670dfbcbe4330e74523549a32f043dfb68da58
aditya puri <puriaditya90@gmail.com>
2026-08-24 21:01:26 +0530

    feat: Add program 19 sum difference and product calculator

---
+++ Basic/19_sum_difference_and_product.py
@@ -0,0 +1,18 @@
+'''Add Two Numbers
+
+Take two numbers from the user and print:
+
+First Number: 10
+Second Number: 20
+Sum: 30
+Difference: -10
+Product: 200
+
+Use arithmetic operators.'''
+
+First_num = int(input("Enter the first number:"))
+second_num = int(input("Enter the second number:"))
+
+print("Sum :", First_num + second_num)
+print("Difference :", First_num - second_num)
+print("Product :", First_num * second_num)
