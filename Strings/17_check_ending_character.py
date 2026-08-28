5b13cfb8c45303c7efe9d3bd8dbfeaf3adfbc77f
aditya puri <puriaditya90@gmail.com>
2026-08-28 16:10:14 +0530

    feat: Add program 17 check ending character

---
+++ Strings/17_check_ending_character.py
@@ -0,0 +1,13 @@
+'''Q17. Check Ending Character
+
+Take a string and check whether it ends with "ing".
+
+Use .endswith().
+'''
+
+text = input("Enter the string: ")
+
+if text.endswith("ing"):
+    print("Ends with ing")
+else:
+    print("Does not end with ing")
