8d33fbfa3d821f3f06358e5f4a42968195276ab6
aditya puri <puriaditya90@gmail.com>
2026-09-02 16:44:14 +0530

    feat: Add program 16 lambda square

---
+++ Functions/16_lambda_square.py
@@ -0,0 +1,10 @@
+"""
+Q16. Lambda Square
+
+Create a lambda function that returns the square of a number.
+Input: 5 -> Output: 25
+"""
+
+square = lambda x: x * x
+
+print("Square of 5:", square(5))
