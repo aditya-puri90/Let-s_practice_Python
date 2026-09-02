5b80bf06ae21c7040895c8e725d3ebe59c0f15c2
aditya puri <puriaditya90@gmail.com>
2026-09-02 16:44:10 +0530

    feat: Add program 01 simple greeting function

---
+++ Functions/01_simple_greeting_function.py
@@ -0,0 +1,16 @@
+"""
+Q1. Simple Function
+
+Create a function greet() that prints:
+"Hello, welcome to Python!"
+
+Call the function 3 times.
+"""
+
+def greet():
+    print("Hello, welcome to Python!")
+
+# Calling the function 3 times
+greet()
+greet()
+greet()
