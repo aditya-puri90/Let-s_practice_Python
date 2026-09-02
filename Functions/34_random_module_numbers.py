27b75cc3b0ee0576ac7540a285c860387138201f
aditya puri <puriaditya90@gmail.com>
2026-09-02 16:44:17 +0530

    feat: Add program 34 random module numbers

---
+++ Functions/34_random_module_numbers.py
@@ -0,0 +1,13 @@
+"""
+Q34. Python Random Module
+
+Demonstrates generating random integers using random.randint().
+"""
+
+import random
+
+num1 = random.randint(1, 100)
+num2 = random.randint(1, 10)
+
+print("Random number (1 - 100):", num1)
+print("Random number (1 - 10): ", num2)
