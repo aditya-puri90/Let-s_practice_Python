a99a57517a13dd220fdb7ba7755820c69e98ccb3
aditya puri <puriaditya90@gmail.com>
2026-09-02 16:44:12 +0530

    feat: Add program 06 student details positional arguments

---
+++ Functions/06_student_details_positional_args.py
@@ -0,0 +1,13 @@
+"""
+Q6. Student Details - Positional Arguments
+
+Create a function student_details(name, age, course)
+and print the student's information using positional arguments.
+"""
+
+def student_details(name, age, course):
+    print("Name:  ", name)
+    print("Age:   ", age)
+    print("Course:", course)
+
+student_details("Aditya", 21, "BSc")
