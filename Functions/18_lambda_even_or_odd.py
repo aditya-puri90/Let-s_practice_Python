a0852eef675c92602866fd18791d88801e4a5811
aditya puri <puriaditya90@gmail.com>
2026-09-02 16:44:14 +0530

    feat: Add program 18 lambda even or odd check

---
+++ Functions/18_lambda_even_or_odd.py
@@ -0,0 +1,10 @@
+"""
+Q18. Lambda Even / Odd Check
+
+Create a lambda function that returns True if a number is even and False otherwise.
+"""
+
+is_even = lambda x: x % 2 == 0
+
+print("Is 24 even?", is_even(24))
+print("Is 25 even?", is_even(25))
