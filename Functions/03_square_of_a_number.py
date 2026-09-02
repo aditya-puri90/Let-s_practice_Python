b5dab72a55c8e1ab924eb42c3ccf593390c9d5ba
aditya puri <puriaditya90@gmail.com>
2026-09-02 16:44:11 +0530

    feat: Add program 03 square of a number

---
+++ Functions/03_square_of_a_number.py
@@ -0,0 +1,13 @@
+"""
+Q3. Return Value - Square of a Number
+
+Create a function square(n) that returns the square of a number.
+Input: 7
+Output: 49
+"""
+
+def square(n):
+    return n * n
+
+result = square(7)
+print("Square:", result)
