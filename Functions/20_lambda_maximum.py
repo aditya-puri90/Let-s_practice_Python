d0d9924cab2dfb352c76e8eb85e9dbbda9cc15e1
aditya puri <puriaditya90@gmail.com>
2026-09-02 16:44:14 +0530

    feat: Add program 20 lambda maximum

---
+++ Functions/20_lambda_maximum.py
@@ -0,0 +1,9 @@
+"""
+Q20. Lambda Maximum
+
+Create a lambda function that takes two numbers and returns the larger one without max().
+"""
+
+maximum = lambda a, b: a if a > b else b
+
+print("Maximum of 10 and 20:", maximum(10, 20))
