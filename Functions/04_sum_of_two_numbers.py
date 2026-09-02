8b3ff032851e5f1bd2791c82776cf047a17dd15e
aditya puri <puriaditya90@gmail.com>
2026-09-02 16:44:11 +0530

    feat: Add program 04 sum of two numbers

---
+++ Functions/04_sum_of_two_numbers.py
@@ -0,0 +1,13 @@
+"""
+Q4. Add Two Numbers
+
+Create a function add(a, b) that takes two numbers and returns their sum.
+Example:
+add(10, 20) -> 30
+"""
+
+def add(a, b):
+    return a + b
+
+result = add(10, 20)
+print("Sum:", result)
