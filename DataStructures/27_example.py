0efb6d9a0d9337d0d3c3f7bce079dc5f3eb2f977
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:49:07 +0530

    feat: Add program 27 print dictionary keys values and items

---
+++ DataStructures/27_example.py
@@ -0,0 +1,34 @@
+'''
+Q27. Print keys and values
+
+Given:
+
+student = {
+    "name": "Aditya",
+    "age": 21,
+    "course": "BSc Computer Science"
+}
+
+Print:
+
+All keys
+All values
+All key-value pairs
+
+Use:
+
+keys()
+values()
+items()
+
+'''
+
+student = {
+    "name": "Aditya",
+    "age": 21,
+    "course": "BSc Computer Science"
+}
+
+print(student.keys())
+print(student.values())
+print(student.items())
