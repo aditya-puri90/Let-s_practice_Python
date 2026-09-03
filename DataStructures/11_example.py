8e5eef864595e5002ad1df82b91805de401c7bef
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:48:09 +0530

    feat: Add program 11 tuple unpacking into variables

---
+++ DataStructures/11_example.py
@@ -0,0 +1,16 @@
+'''
+Q11. Tuple unpacking
+
+Given:
+
+student = ("Aditya", 21, "BSc Computer Science")
+
+Unpack the tuple into three variables and print them.
+'''
+student = ("Aditya", 21, "BSc Computer Science")
+
+name, age, course = student
+
+print("Name:",name)
+print("Age:",age)
+print("course:",course)
