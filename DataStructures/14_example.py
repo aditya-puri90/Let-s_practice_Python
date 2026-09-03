bdf5b79359c449a906485887e061d465309f1f71
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:48:17 +0530

    feat: Add program 14 maximum and minimum in tuple

---
+++ DataStructures/14_example.py
@@ -0,0 +1,14 @@
+'''
+Q14. Find maximum and minimum in tuple
+
+Given:
+
+numbers = (45, 12, 78, 34, 89, 23)
+
+Find the maximum and minimum values.
+'''
+
+numbers = (45, 12, 78, 34, 89, 23)
+
+print("Maximum:",max(numbers))
+print("Minimum:",min(numbers))
