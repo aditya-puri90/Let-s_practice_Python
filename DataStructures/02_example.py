466560e1467c43b4aa6a8872558f019236e99995
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:47:30 +0530

    feat: Add program 02 access list elements

---
+++ DataStructures/02_example.py
@@ -0,0 +1,19 @@
+'''
+Q2. Access list elements
+
+Given:
+
+numbers = [10, 20, 30, 40, 50]
+
+Print:
+
+First element
+Last element
+Third element
+'''
+
+numbers = [10,20,30,40,50]
+
+print(numbers[0])
+print(numbers[-1])
+print(numbers[2])
