281f4ed6de8ea5067893d601aeaffa65e6a39f99
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:49:01 +0530

    feat: Add program 25 update dictionary value

---
+++ DataStructures/25_example.py
@@ -0,0 +1,21 @@
+'''
+Q25. Update a dictionary value
+
+Given:
+
+student = {
+    "name": "Aditya",
+    "age": 21,
+    "course": "BSc Computer Science"
+}
+
+Change the age to 22.
+'''
+student = {
+    "name": "Aditya",
+    "age": 21,
+    "course": "BSc Computer Science"
+}
+
+student["age"]=22
+print(student)
