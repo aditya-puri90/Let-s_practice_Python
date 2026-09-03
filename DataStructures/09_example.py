b67baedb702bb04de3ebb206a060c71de96006ab
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:47:50 +0530

    feat: Add program 09 create and print tuple

---
+++ DataStructures/09_example.py
@@ -0,0 +1,13 @@
+'''
+Q9. Create a tuple
+
+Create a tuple containing five programming languages:
+
+Python, Java, C++, JavaScript, SQL
+
+Print the tuple.
+'''
+
+languages = ( "Python","Java","Sql","C++","c")
+
+print(languages)
