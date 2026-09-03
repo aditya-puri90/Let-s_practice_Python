af946f27aa1e8d001dfd0d2f193e869919e41eec
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:48:26 +0530

    feat: Add program 17 add and remove elements in set

---
+++ DataStructures/17_example.py
@@ -0,0 +1,15 @@
+'''
+Q17. Add and remove
+
+Given:
+
+fruits = {"apple", "banana", "mango"}
+Add "orange"
+Remove "banana"
+Print the set
+'''
+fruits = {"apple", "banana", "mango"}
+
+fruits.add("orange")
+fruits.remove("banana")
+print(fruits)
