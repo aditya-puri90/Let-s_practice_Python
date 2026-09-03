914f6a1c63d81f706e5cd591a46b66cecba5e351
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:47:32 +0530

    feat: Add program 03 update list element

---
+++ DataStructures/03_example.py
@@ -0,0 +1,13 @@
+'''
+Q3. Update a list
+
+Given:
+
+fruits = ["apple", "banana", "mango", "orange"]
+
+Change "banana" to "grapes" and print the list.
+'''
+fruits = ["apple", "banana", "mango", "orange"]
+
+fruits[1]="grapes"
+print(fruits)
