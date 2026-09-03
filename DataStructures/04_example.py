ea0ad2fb204e1530b56476daa0ff51fcbf07cc8b
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:47:35 +0530

    feat: Add program 04 append elements to list

---
+++ DataStructures/04_example.py
@@ -0,0 +1,15 @@
+'''
+Q4. Add elements
+
+Given:
+
+numbers = [10, 20, 30]
+
+Add 40 and 50 to the list using append().
+'''
+
+numbers = [10,20,30]
+
+numbers.append(40)
+numbers.append(50)
+print(numbers)
