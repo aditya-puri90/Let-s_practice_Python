b405201e5e2e7d67d91efcfa271dbebdc1c0ec01
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:49:04 +0530

    feat: Add program 26 delete item from dictionary

---
+++ DataStructures/26_example.py
@@ -0,0 +1,25 @@
+'''
+Q26. Delete a dictionary item
+
+Given:
+
+student = {
+    "name": "Aditya",
+    "age": 21,
+    "course": "BSc Computer Science"
+}
+
+Delete the "age" key.
+
+
+'''
+
+student = {
+    "name": "Aditya",
+    "age": 21,
+    "course": "BSc Computer Science"
+}
+
+del student["age"]
+
+print(student)
