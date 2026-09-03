860d966ccf4b10f7ab5f1a63e6aff007dbba123a
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:48:15 +0530

    feat: Add program 13 convert list to tuple

---
+++ DataStructures/13_example.py
@@ -0,0 +1,14 @@
+'''
+Q13. Convert list to tuple
+
+Given:
+
+numbers = [10, 20, 30, 40, 50]
+
+Convert it into a tuple.
+'''
+
+numbers = [10, 20, 30, 40, 50]
+
+numbers_tuple =tuple(numbers)
+print(numbers_tuple)
