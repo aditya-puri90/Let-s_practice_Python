0f31c6a805e83005bfb262f4eeeda72da204e081
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:48:32 +0530

    feat: Add program 19 set intersection operation

---
+++ DataStructures/19_example.py
@@ -0,0 +1,17 @@
+'''
+Q19. Intersection
+
+Using:
+
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+Find the common elements.
+'''
+
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+intersection = A.intersection(B)
+print(intersection)
+
