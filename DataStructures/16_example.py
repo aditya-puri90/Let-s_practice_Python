66a797ff1c60aa8efe9a25921ad2a71fd02fe518
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:48:23 +0530

    feat: Add program 16 remove duplicates from list using set

---
+++ DataStructures/16_example.py
@@ -0,0 +1,18 @@
+'''
+Q16. Remove duplicates
+
+Given:
+
+numbers = [10, 20, 10, 30, 20, 40, 10, 50]
+
+Remove duplicate values using a set.
+
+Expected unique values:
+
+10, 20, 30, 40, 50
+'''
+numbers = [10, 20, 10, 30, 20, 40, 10, 50]
+
+unique_numbers= set(numbers)
+
+print(unique_numbers)
