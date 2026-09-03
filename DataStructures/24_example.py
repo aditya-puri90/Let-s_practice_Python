c8926cda3e6c35a0dce1132c23f0cdfbd0459fed
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:48:58 +0530

    feat: Add program 24 add item to dictionary

---
+++ DataStructures/24_example.py
@@ -0,0 +1,23 @@
+'''
+Q24. Add a dictionary item
+
+Given:
+
+student = {
+    "name": "Aditya",
+    "age": 21
+}
+
+Add:
+
+"city": "Pune"
+'''
+
+student = {
+    "name": "Aditya",
+    "age": 21
+}
+
+student ["city"]="pune"
+
+print(student)
