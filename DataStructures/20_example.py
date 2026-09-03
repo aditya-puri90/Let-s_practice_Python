64279a9540379dcb8ed1120f3f7c42aa9d1d7cf9
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:48:34 +0530

    feat: Add program 20 set difference operations

---
+++ DataStructures/20_example.py
@@ -0,0 +1,19 @@
+'''
+Q20. Difference
+
+Using the same sets:
+
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+Find:
+
+A - B
+B - A
+'''
+
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6}
+
+print("difference:",A-B)
+print("Difference:",B-A)
