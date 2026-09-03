8cd7cb2d469d8683be6813e9bfc1916d64cf081c
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:47:38 +0530

    feat: Add program 05 insert element into list

---
+++ DataStructures/05_example.py
@@ -0,0 +1,14 @@
+'''
+Q5. Insert an element
+
+Given:
+
+fruits = ["apple", "banana", "orange"]
+
+Insert "mango" between "banana" and "orange".
+'''
+
+fruits = ["apple", "banana", "orange"]
+
+fruits.insert(2,"mango")
+print(fruits)
