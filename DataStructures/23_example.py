a363b4fe7e98e9f701e837413b268747b46c6c1b
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:48:56 +0530

    feat: Add program 23 access dictionary values

---
+++ DataStructures/23_example.py
@@ -0,0 +1,28 @@
+'''
+Q23. Access dictionary values
+
+Given:
+
+student = {
+    "name": "Aditya",
+    "age": 21,
+    "course": "BSc Computer Science"
+}
+
+Print the student's:
+
+Name
+Age
+Course
+'''
+
+student = {
+    "name": "Aditya",
+    "age": 21,
+    "course": "BSc Computer Science"
+}
+
+
+print("Name:",student["name"])
+print("Age:",student["age"])
+print("Course:",student["course"])
