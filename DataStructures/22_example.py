0818ae885edae5aec3bca9cd71898993eb5ec913
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:48:53 +0530

    feat: Add program 22 create and print dictionary

---
+++ DataStructures/22_example.py
@@ -0,0 +1,16 @@
+'''
+
+Q22. Create a dictionary
+
+Create a dictionary containing:
+
+name → Aditya
+age → 21
+course → BSc Computer Science
+
+Print the dictionary.
+'''
+
+dict = {"name":"Aditya", "Age":21,"Course":"Bsc"}
+
+print(dict)
