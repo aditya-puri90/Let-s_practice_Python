780dbadcc6df6877d5a282d0f3f6a3a9f14f48c2
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:47:44 +0530

    feat: Add program 07 find largest and smallest numbers in list

---
+++ DataStructures/07_example.py
@@ -0,0 +1,14 @@
+'''
+Q7. Find largest and smallest
+
+Given:
+
+numbers = [45, 12, 78, 34, 89, 23]
+
+Find the largest and smallest number.
+'''
+
+numbers = [45, 12, 78, 34, 89, 23]
+
+print("Largest:",max(numbers))
+print("Samllest:",min(numbers))
