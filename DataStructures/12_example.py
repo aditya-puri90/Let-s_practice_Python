375f71bd47945739ad27cdff4c6ac54c0cad5e49
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:48:12 +0530

    feat: Add program 12 count and index methods in tuple

---
+++ DataStructures/12_example.py
@@ -0,0 +1,18 @@
+'''
+Q12. Count and index
+
+Given:
+
+numbers = (10, 20, 10, 30, 10, 40)
+
+Find:
+
+How many times 10 occurs
+The index of the first 30
+'''
+
+numbers = (10, 20, 10, 30, 10, 40)
+
+
+print(numbers.count(10))
+print(numbers.index(30))
