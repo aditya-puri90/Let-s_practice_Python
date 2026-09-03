23be1805de38787cb9d53f4fd358d98108c2cc18
aditya puri <puriaditya90@gmail.com>
2026-09-03 16:47:53 +0530

    feat: Add program 10 access tuple elements and slicing

---
+++ DataStructures/10_example.py
@@ -0,0 +1,19 @@
+'''
+Q10. Access tuple elements
+
+Given:
+
+languages = ("Python", "Java", "C++", "JavaScript", "SQL")
+
+Print:
+
+First element
+Last element
+Second and third elements
+'''
+
+languages = ("Python", "Java", "C++", "JavaScript", "SQL")
+
+print(languages[0])
+print(languages[-1])
+print(languages[1:3])
